cd4f826820b9bd5761ae55d5c1580856f6eada53
Zanxiang Wang <zanxiang.wang@outlook.com>
2024-10-09 18:20:51 -0500

    ci/cd config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Enables React's strict mode for additional checks
+    swcMinify: true, // Uses the SWC compiler for faster builds and minification
+    images: {
+      domains: ['example.com'], // Replace with domains you use for external images
+    },
+    output: 'standalone', // Optimizes the build for use in a Docker container
+  };
+  
+  export default nextConfig;
+  
